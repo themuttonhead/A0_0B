5d86d533ab4ff66d6967046f5d13a73618b4ffb1
themuttonhead <35484066+themuttonhead@users.noreply.github.com>
2018-01-16 15:05:46 +0530

    Add files via upload

---
+++ 2nd prog.py
@@ -0,0 +1,5 @@
+#addition of 2 numbers
+a=107000340006007008
+b=2023111888333999444000555
+c=a+b
+print("addition of",a,"and",b,"is",c)
---
+++ arthamatic.py
@@ -0,0 +1,4 @@
+a=100
+b=200
+
+print("subtraction of",b,"-",a,"=",b-a)
