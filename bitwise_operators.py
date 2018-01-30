6ab8675be879ddf46d011a9e5f0e5fd5e403c004
themuttonhead <35484066+themuttonhead@users.noreply.github.com>
2018-01-30 15:14:00 +0530

    Add files via upload

---
+++ bitwise_operators.py
@@ -0,0 +1,8 @@
+#bitwise_operators
+a=20
+b=30
+print(~b)
+print(~a)
+print(a&b)
+print(a|b)
+
---
+++ boolian_ops.py
@@ -0,0 +1,11 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if a==b and b==c and a==c:
+    print("yay")
+else:
+    print("nah")
+if a==b or b==c or a==c:
+    print("yay")
+else:
+    print ("nah")
