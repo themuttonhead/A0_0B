b6d8c5ba4fd080ff1808191c8270468e3a6cd64b
Abey Antony <35484066+themuttonhead@users.noreply.github.com>
2020-10-22 10:24:07 +0530

    Update email_sheduler1.py

--- email_sheduler1.py
+++ email_sheduler1.py
@@ -2,7 +2,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("abey.17ec@cmr.edu.in", "abey@cmr")
+server.login("abey.17ec@cmr.edu.in", "abeycmr")
 msg = "hello"
 server.sendmail("abey.17ec@cmr.edu.in", "abeykakkuzhi@gmail.com", msg)
 server.quit()
